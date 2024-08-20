fca3591073a5146f513d7df38d94405e3eb89d25
Olzah <oleh.zaho@gmail.com>
2024-08-20 12:54:07 +0000

    I don't know why it doesn't work

---
+++ addons/sale_order_global_stock_rule/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Poznan 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Global Stock Route",
+    "summary": "Add the possibility to choose one warehouse path for an order",
+    "version": "17.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Warehouse",
+    "website": "https://ventor.tech/",
+    "author": "Poznan, VentorTech, Olzah",
+    "license": "AGPL-3",
+    "installable": True,
+    # "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml"],
+}
